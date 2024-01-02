b662692bdbcb9c4fe2f9f3c09ff0513ec7a5c9c9
JordanWalling <jordanwalling1991@gmail.com>
2024-01-02 17:36:50 +1100

    complete array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(array, string) {
+  const matchingDrivers = array.filter((drivers) => {
+    return drivers.toLowerCase() === string.toLowerCase();
+  });
+  return matchingDrivers;
+}
+
+function fuzzyMatch(array, string) {
+  let match = array.filter((drivers) => {
+    // return drivers.includes(string);
+    return drivers[0] === string[0];
+  });
+  return match;
+}
+
+function matchName(array, string) {
+  let nameMatch = array.filter((driver) => {
+    return driver.name === string;
+  });
+  return nameMatch;
+}
